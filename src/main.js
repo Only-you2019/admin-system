78e1802a7847bb0f20783ebf7f726848b7a7e1e1
Olny-you2019 <2669986522@qq.com>
2019-07-16 23:31:32 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,11 +3,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+//引入jquery
+import $ from 'jquery'
+//引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 引入echarts
+import echarts from 'echarts'
 
-Vue.config.productionTip = false
+Vue.prototype.$echarts = echarts;
+
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
